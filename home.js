d514da0f18c24ce9acb45ba13c8af46c2e48dd01
Andrew Kotey <andrewkotey@andrews-mbp.otterbein.edu>
2017-02-21 08:52:27 -0500

    added home.js and folders for node

---
+++ home.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+var amqp = require('amqplib/callback_api');
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json());//for parsing application/json
+app.use(bodyParser.urlencoded({extended: true}));//for parsing application/x-ww-form-urlencoded
+app.use(upload.array()); // for parsing multipart/form-data
+app.use(express.static('public'));
+
+app.post('/', function(req, res){
+
+	amqp.connect('amqp://localhost', function(err, conn) {
+  	conn.createChannel(function(err, ch) {
+    var q = 'hello';
+    var msg = JSON.stringify(req.body.URL);
+   	var noQuote = msg.split('"').join('');
+   	var msg2 = JSON.stringify(req.body.term);
+   	var noQuote2 = msg2.split('"').join('');
+
+    ch.assertQueue(q, {durable: false});
+    // Note: on Node 6 Buffer.from(msg) should be used
+    ch.sendToQueue(q, new Buffer(noQuote));
+    ch.sendToQueue(q, new Buffer(noQuote2));
+    console.log(" [x] Sent %s", noQuote);
+    console.log(" [x] Sent %s", noQuote2);
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 3001);
+});
+
+	res.send("recieved your request");
+
+});
+
+app.get('/', function(req, res){
+    res.render('form');
+});
+
+app.listen(3001)
